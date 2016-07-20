f6dec750828f50b8ccdf867406276ba8510054d5
Viliam Kopecký <vk@viliamkopecky.cz>
2016-07-20 15:36:35 +0200

    Button with validations

--- src/Button.php
+++ src/Button.php
@@ -2,47 +2,80 @@
 
 namespace Mangoweb\Messenger;
 
+use Nette\Utils\Validators;
+use Nette\Utils\AssertionException;
+
 class Button {
 
 	const TYPE_WEB_URL = 'web_url';
 	const TYPE_POSTBACK = 'postback';
 	const TYPE_PHONE_NUMBER = 'phone_number';
 
+	const MAX_TITLE_CHARS = 20;
+	const MAX_PAYLOAD_CHARS = 1000;
+
 	public $type = self::TYPE_WEB_URL;
 	public $title; // max 20 chars
 	public $url;
 	public $payload; // max 1000 chars
 
+	public static function sanitizeType($value) {
+		if(!Utils::inArray($value, [ self::TYPE_WEB_URL, self::TYPE_POSTBACK, self::TYPE_PHONE_NUMBER ])) {
+			throw new AssertionException("The type is out of valid range.");
+		}
+		return $value;
+	}
+
+	public static function sanitizeTitle($value) {
+		Validators::assert($value, 'string:1..' . self::MAX_TITLE_CHARS, 'title');
+		return $value;
+	}
+
+	public static function sanitizeUrl($value) {
+		Validators::assert($value, 'url', 'url');
+		return $value;
+	}
+
+	public static function sanitizePayload($value) {
+		Validators::assert($value, 'string:1..' . self::MAX_PAYLOAD_CHARS, 'payload');
+		return $value;
+	}
+
+	public static function sanitizePhoneNumber($value) {
+		Validators::assert($value, 'pattern:\\+[0-9]{6,15}', 'phoneNumber');
+		return $value;
+	}
+
 	public static function url($title, $url) {
 		$button = new self;
 		$button->type = self::TYPE_WEB_URL;
-		$button->title = $title;
-		$button->url = $url;
+		$button->title = self::sanitizeTitle($title);
+		$button->url = self::sanitizeUrl($url);
 		return $button;
 	}
 
 	public static function postback($title, $payload) {
 		$button = new self;
 		$button->type = self::TYPE_POSTBACK;
-		$button->title = $title;
-		$button->payload = $payload;
+		$button->title = self::sanitizeTitle($title);
+		$button->payload = self::sanitizePayload(Utils::serialize($payload));
 		return $button;
 	}
 
 	public static function phone($title, $phone_number) {
 		$button = new self;
 		$button->type = self::TYPE_PHONE_NUMBER;
-		$button->title = $title;
-		$button->phone_number = $phone_number;
+		$button->title = self::sanitizeTitle($title);
+		$button->phone_number = self::sanitizePhoneNumber($phone_number);
 		return $button;
 	}
 
 	public function toSchema() {
 		$schema = [
-			'type' => $this->type,
+			'type' => self::sanitizeType($this->type),
 			'title' => $this->title,
 			'url' => $this->url,
-			'payload' => Utils::serialize($this->payload),
+			'payload' => $this->type === self::TYPE_PHONE_NUMBER ? $this->phone_number : Utils::serialize($this->payload),
 		];
 		$schema = array_filter($schema);
 		return $schema;
---
+++ tests/cases/Button.phpt
@@ -0,0 +1,51 @@
+<?php
+
+use Mangoweb\Messenger\Button;
+use Tester\Assert;
+
+require __DIR__ . '/../bootstrap.php';
+
+$button = Button::url('title', 'https://fb.com/');
+
+Assert::equal([
+	'type' => 'web_url',
+	'title' => 'title',
+	'url' => 'https://fb.com/'
+], $button->toSchema());
+
+Assert::exception(function() {
+	Button::url('title', 'invalid url');
+}, 'Nette\Utils\AssertionException', "The url expects to be url, string 'invalid url' given.");
+
+$button = Button::postback('title', 'xxxxxxxxxx');
+
+Assert::equal([
+	'type' => 'postback',
+	'title' => 'title',
+	'payload' => 'xxxxxxxxxx'
+], $button->toSchema());
+
+Assert::exception(function() {
+	$longString = str_repeat('x', 1100);
+	Button::postback('title', $longString);
+}, 'Nette\Utils\AssertionException', "The payload expects to be string in range 1..1000, string given.");
+
+$button = Button::phone('title', '+420739123456');
+
+Assert::equal([
+	'type' => 'phone_number',
+	'title' => 'title',
+	'payload' => '+420739123456'
+], $button->toSchema());
+
+Assert::exception(function() {
+	Button::phone('title', '123456789');
+}, 'Nette\Utils\AssertionException', "The phoneNumber expects to be pattern in range \+[0-9]{6,15}, string '123456789' given.");
+
+Assert::exception(function() {
+	Button::phone('title', '+1');
+}, 'Nette\Utils\AssertionException', "The phoneNumber expects to be pattern in range \+[0-9]{6,15}, string '+1' given.");
+
+Assert::exception(function() {
+	Button::phone('title', '+abcdefghi');
+}, 'Nette\Utils\AssertionException', "The phoneNumber expects to be pattern in range \+[0-9]{6,15}, string '+abcdefghi' given.");
